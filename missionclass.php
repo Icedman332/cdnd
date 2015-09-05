918cc1ba2a74aadc7ab3b0265abc947db1b067e2
SataiJhadur <Mudd_dawg_us@yahoo.com>
2015-09-05 16:39:42 -0600

    set mission working

--- missionclass.php
+++ missionclass.php
@@ -4,7 +4,7 @@
 class mission
 {
 	public $mission_time = 12;
-	public $mission_troops = array();
+	//public $mission_troops = array();
 	public $mission_type = 'scouting';
 	public $start_date = 1;
 	public $end_date = 0;
@@ -14,17 +14,35 @@ class mission
 	public $reward = array();
 	public $XP_earned = 12;
 	
+	public function setmission($time,$date,$troops,$type,$item)
+	{
+		$this->mission_time = $time;
+		$this->start_date = $date;
+		$this->enddate($time,$date);
+		$this->food_cost = $troops*10;
+		$this->mission_type = $type;
+		$this->cargo = $item;
+		$this->XP_earned=$time*1;
+	}
 	public function enddate($mision_time1,$stat_date1) //gets end date
 	{
 		$this->end_date = $stat_date1+$mision_time1;
 		return $this->end_date;
 	}
+	public function setreward()
+	{
+		
+	}
 
 }
 
-$test = new mission;
-$test->enddate(15,1);
+$test = new mission; 
+$test->setmission(15,1,5,'guard','none');
+$test->setreward();
 echo $test->end_date;
 echo $test->start_date;
 echo $test->mission_time;
+echo $test->food_cost;
+echo $test->mission_type;
+echo $test->cargo;
 ?>
