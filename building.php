e7ec351451ee5e342a31dfcada1d7a17f314bf39
SataiJhadur <Mudd_dawg_us@yahoo.com>
2015-09-05 11:36:50 -0600

    new class

---
+++ building.php
@@ -0,0 +1,24 @@
+
+<?php
+
+class building
+{
+	public $name_building = 'new building';
+	public $build_cost = 500;
+	public $build_ability = array();
+	public $gold_upkeep = 50;
+	public $production_upkeep = 50;
+	public $working_people = 3;
+	public $battle_building = true;
+	public $wonder_building = true;
+	public $happiness_mod = 2;
+	public $gold_mod = 50;
+	public $production_mod = 50;
+	public $growth_mod = .01;
+	public $buy_dc = 1;
+	public $crime_mod = 1;
+	public $tec_needed = 'arcane t1';
+	
+}
+
+?>
---
+++ statclass.php
@@ -0,0 +1,23 @@
+
+<?php
+
+class stats 
+{
+	public $str = 10;
+	public $dex = 10;
+	public $con = 10;
+	public $int = 10;
+	public $wis = 10;
+	public $cha = 10;
+	public $proficiency_bonus = 2;
+	public $proficent_with = array();
+	public $training_time = 8;
+	public $training_cost = 80;
+	public $monthy_cost = 10;
+	public $class_ability = array();
+	Public $spell_ability = array();
+	public $prime_stat = str;
+	public $secon_stat = dex;
+	public $third_stat = con;
+}
+?>
