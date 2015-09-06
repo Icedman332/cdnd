2f16452041785b0ea99848f387d6225dcbe323e2
SataiJhadur <Mudd_dawg_us@yahoo.com>
2015-09-05 18:19:04 -0600

    called done

--- missionclass.php
+++ missionclass.php
@@ -11,38 +11,62 @@ class mission
 	public $cargo = 'info';
 	public $food_cost = 10;
 	public $total_troops =1;
-	public $reward = array();
+	public $Rtype = array('gold','item','people','tec','ore type','plant','animal','dragon');
+	public $reward = array(0,'none',0,'none','none','none','none','none');
 	public $XP_earned = 12;
+	
 	
 	public function setmission($time,$date,$troops,$type,$item)
 	{
 		$this->mission_time = $time;
 		$this->start_date = $date;
 		$this->enddate($time,$date);
-		$this->food_cost = $troops*10;
 		$this->mission_type = $type;
 		$this->cargo = $item;
-		$this->XP_earned=$time*1;
+		if($type=='guard')
+		{
+			$this->food_cost = $troops*0;
+			$this->XP_earned=$time*.2;
+			$this->setreward(0,'no',0,'n/a','n/a','n/a','n/a','n/a');
+			echo $this->XP_earned;
+		}
+		else 
+		{
+			$this->food_cost = $troops*10;
+			$this->XP_earned=$time*1;
+			$this->setreward(0,'iron sword',500,'n/a','n/a','n/a','n/a','adult red');
+			echo $this->XP_earned;
+		}
 	}
 	public function enddate($mision_time1,$stat_date1) //gets end date
 	{
 		$this->end_date = $stat_date1+$mision_time1;
 		return $this->end_date;
 	}
-	public function setreward()
+	public function setreward($gold,$item,$people,$tec,$good,$plant,$animal,$dragon) 
 	{
+		$this->reward[0] = $gold;
+		$this->reward[1] = $item;
+		$this->reward[2] = $people;
+		$this->reward[3] = $tec;
+		$this->reward[4] = $good;
+		$this->reward[5] = $plant;
+		$this->reward[6] = $animal;
+		$this->reward[7] = $dragon;
 		
+		for($roll=0;$roll<=7;$roll++)
+		{
+			echo $this->reward [$roll]. " " . $this->Rtype[$roll]. "<br>";
+			$this->roll=$roll+1;
+		}
 	}
 
 }
 
+
 $test = new mission; 
-$test->setmission(15,1,5,'guard','none');
-$test->setreward();
-echo $test->end_date;
-echo $test->start_date;
-echo $test->mission_time;
-echo $test->food_cost;
-echo $test->mission_type;
-echo $test->cargo;
+$test->setmission(15,1,5,'spying','none');
+//$test->setreward(15,'iron sword',0,'n/a','n/a','n/a','n/a');
+
+
 ?>
